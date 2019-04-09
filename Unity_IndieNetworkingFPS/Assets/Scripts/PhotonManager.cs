6c1a325fc5d744e6e6afa7fe6c783a063c99dc20
Thimo Reumerman <33859063+ThimoReumerman@users.noreply.github.com>
2019-04-09 11:39:25 +0200

    Changed some stuff

--- Unity_IndieNetworkingFPS/Assets/Scripts/PhotonManager.cs
+++ Unity_IndieNetworkingFPS/Assets/Scripts/PhotonManager.cs
@@ -4,23 +4,29 @@ using UnityEngine;
 
 public class PhotonManager : MonoBehaviour {
     public string gameVersion = "1.0";
-    public AnimationClip loadingClip;
-    public Animator loadingAnimator;
+    public string mainRoomName = "Main Room";
 
     public void Connect () {
-        PhotonNetwork.ConnectUsingSettings(gameVersion);
-        StartCoroutine(WaitForConnect());
-
+        PhotonNetwork.ConnectUsingSettings (gameVersion);
+        StartCoroutine (WaitForConnect ());
     }
 
-    public IEnumerator WaitForConnect() {
+    public IEnumerator WaitForConnect () {
 
-        while(!PhotonNetwork.connectedAndReady) {
+        while (!PhotonNetwork.connectedAndReady) {
             yield return null;
         }
 
-        print("You did it!");
+        print ("You did it!");
+        RoomManager();
         yield break;
+    }
+
+    void RoomManager () {
+        RoomOptions roomOptions = new RoomOptions ();
+        roomOptions.MaxPlayers = 0;
+        roomOptions.IsOpen = true;
+        PhotonNetwork.JoinOrCreateRoom (mainRoomName, roomOptions, TypedLobby.Default);
     }
 
 }
