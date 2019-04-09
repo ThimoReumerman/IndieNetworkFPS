08f2a6c25a8d48b6f75c939ad0fdafadf611d530
Thimo Reumerman <33859063+ThimoReumerman@users.noreply.github.com>
2019-04-09 11:14:15 +0200

    Networking
    Added main scene where to connect to the Photon server

---
+++ Unity_IndieNetworkingFPS/Assets/Scripts/PhotonManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonManager : MonoBehaviour {
+    public string gameVersion = "1.0";
+    public AnimationClip loadingClip;
+    public Animator loadingAnimator;
+
+    public void Connect () {
+        PhotonNetwork.ConnectUsingSettings(gameVersion);
+        StartCoroutine(WaitForConnect());
+
+    }
+
+    public IEnumerator WaitForConnect() {
+
+        while(!PhotonNetwork.connectedAndReady) {
+            yield return null;
+        }
+
+        print("You did it!");
+        yield break;
+    }
+
+}
